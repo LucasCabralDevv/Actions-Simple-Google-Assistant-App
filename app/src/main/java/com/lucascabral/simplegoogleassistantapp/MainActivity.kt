336127d3943311ac172aafb3faf02fad24e4c89b
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-08-31 18:01:48 -0300

    Actions criado para features Profile, Settings, Records. Falta fazer upload do app para google play.

--- app/src/main/java/com/lucascabral/simplegoogleassistantapp/MainActivity.kt
+++ app/src/main/java/com/lucascabral/simplegoogleassistantapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.lucascabral.simplegoogleassistantapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        intent.handleIntent()
+    }
+
+    private fun Intent.handleIntent() {
+
+        when(action) {
+            Intent.ACTION_VIEW -> handleDeepLink(data)
+            else -> goToDefaultView()
+        }
+    }
+
+    private fun handleDeepLink(data: Uri?) {
+
+        when(data?.path) {
+            "/open" -> {
+                val featureType = data.getQueryParameter("featureName").orEmpty()
+                navigateToActivity(featureType)
+            }
+        }
+    }
+
+    private fun navigateToActivity(featureType: String) {
+        when(featureType) {
+            "profile" -> {
+                val profileIntent = Intent(this, ProfileActivity::class.java)
+                startActivity(profileIntent)
+            }
+            "settings" -> {
+                val settingsIntent = Intent(this, SettingsActivity::class.java)
+                startActivity(settingsIntent)
+            }
+        }
+    }
+
+    private fun goToDefaultView() {
+
     }
 }
